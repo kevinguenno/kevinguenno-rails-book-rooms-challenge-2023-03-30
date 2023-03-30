c2f74c3eb77ea49c991ff94212cad35752f7a38a
kevinguenno <kevin.guenno@gmail.com>
2023-03-30 18:00:25 +0200

    flat picker en cours, Front en cours

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  connect() {
+    new flatpickr(this.element, {
+      // enableTime: true,
+      mode: "range",
+      minDate: "today",
+      dateFormat: "d-m-Y"
+
+    });
+  }
+}
